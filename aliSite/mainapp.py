6c8ffdadf1e49171f5906acd9e5a0c151439d095
ahmed ashref <ahmed.ashref.sw@gmail.com>
2020-04-30 10:45:36 +0200

    SetupAgain

---
+++ aliSite/mainapp.py
@@ -0,0 +1,15 @@
+import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return flask.render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
+
